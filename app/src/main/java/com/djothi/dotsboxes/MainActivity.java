d02fd20b179cfe4af6504f2a04167f19f3280e07
DJ <dilvan.jothi@hotmail.com>
2020-05-12 01:51:04 +0100

    Completed Front Page

--- app/src/main/java/com/djothi/dotsboxes/MainActivity.java
+++ app/src/main/java/com/djothi/dotsboxes/MainActivity.java
@@ -2,13 +2,26 @@ package com.djothi.dotsboxes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
+        try {
+            synchronized (this){
+                wait(10);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
