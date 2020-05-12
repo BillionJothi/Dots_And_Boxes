6ec90e29e3b0e6fa89b0aae8ab3687dcb1be6ccd
DJ <dilvan.jothi@hotmail.com>
2020-05-12 06:03:00 +0100

    Completed 1st Fragment for Game Options

--- app/src/main/java/com/djothi/dotsboxes/LocalGame1.java
+++ app/src/main/java/com/djothi/dotsboxes/LocalGame1.java
@@ -3,12 +3,16 @@ package com.djothi.dotsboxes;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.Choreographer;
 
 import java.util.Objects;
 
 public class LocalGame1 extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,8 @@ public class LocalGame1 extends AppCompatActivity {
             ab.setTitle(R.string.vs_computer);
             ab.setDisplayHomeAsUpEnabled(true);
         }
+
+
 
 
 
